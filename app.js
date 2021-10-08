96e13045365d77c3fc3350bc0abcc6f861bc27e7
DParkinson-BYUI <parkidr@gmail.com>
2021-10-08 15:45:52 -0700

    completed mongoose

--- app.js
+++ app.js
@@ -1,40 +1,75 @@
 const path = require('path');
+const keys = require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const cors = require('cors');
+const corsOptions = {
+  origin: "dparki2-cse341ecommerce.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
 const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+
+// const optons = {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   userCreateIndex: true,
+//   useFindAndModify: false,
+//   family: 4
+// };
+
+const MONGODB_URL = process.env.MONGODB_URL;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById(1)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
+app.use(async (req, res, next) => {
+  try{
+    const user = await User.findById('6160a8c1d4db714ef99ae0bf');
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+ });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(cors(corsOptions));
 
 app.use(errorController.get404);
 
-app.listen(3000);
+async function init() {
+  try {
+    await mongoose.connect(MONGODB_URL);
+    const user = await User.findOne();
+    if (!user) {
+      const user = new User({
+        name: 'Drew',
+        email: 'dparki2@yahoo.com',
+        cart: {
+          items: [],
+        },
+      });
+      await user.save();
+    }
+    app.listen(process.env.PORT || 3000);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
+
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description
-    })
+  const product = new Product({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+    userId: req.user
+  });
+  product
+    .save()
     .then(result => {
       // console.log(result);
       console.log('Created Product');
@@ -36,11 +38,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
+  Product.findById(prodId)
     // Product.findById(prodId)
-    .then(products => {
-      const product = products[0];
+    .then(product => {
       if (!product) {
         return res.redirect('/');
       }
@@ -60,14 +60,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findById(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
+
+  Product.findById(prodId).then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl;
+    return product.save()
+   })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -76,9 +76,10 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+  Product.find()
+  // .populate('userId')
     .then(products => {
+      console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
@@ -90,11 +91,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId)
-    .then(product => {
-      return product.destroy();
-    })
-    .then(result => {
+  Product.findByIdAndRemove(prodId)
+    .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
